94de98144e4c658da4d9bf52a93bdc8cb9207403
Chivas-Regal <1411390466@qq.com>
2023-06-23 11:23:34 +0800

    udpate acforces

--- acforces.py
+++ acforces.py
@@ -26,6 +26,7 @@ browser = None
 
 from html.parser import HTMLParser
 
+# 从 <div class="input"> 或者 <div class="output"> 中解析样例
 class SampParser(HTMLParser):
     def __init__(self, folder, file):
         HTMLParser.__init__(self)
@@ -53,15 +54,19 @@ class SampParser(HTMLParser):
             self.sam_in_text.write(data.encode('utf-8'))
 
 
+# 用户信息 ('username', 'password', 'contest_id')
 info_data = {}
 
 def handle_Login (username, password):
+    print("%s> \033[33mLogining...\033[0m"%info_data['username'])
     global browser
 
     import requests
     from robobrowser import RoboBrowser
     import pickle
 
+    # 本工程目录下存在 cookie.pkl 文件，就使用 cookie 登录
+    #                          否则  ，使用账密登录
     if os.path.isfile('%s/cookie.pkl'%dir_path):
         with open('%s/cookie.pkl'%dir_path, 'rb') as f:
             cookies = pickle.load(f)
@@ -76,8 +81,10 @@ def handle_Login (username, password):
         enter_form['password'] = password
         browser.submit_form(enter_form)
 
+    # 侧边栏，第二块内是否存在用户名，存在则表示登陆成功
     div_sidbar_tags = browser.find_all('div', {'class': 'caption titled'})
     if len(div_sidbar_tags) >= 2 and username in div_sidbar_tags[1].text:
+        # 登录成功后再把 cookie 写入 cookie.pkl 中
         with open('%s/cookie.pkl'%dir_path, 'wb') as f:
             pickle.dump(browser.session.cookies, f)
         return 1
@@ -85,22 +92,33 @@ def handle_Login (username, password):
         print('\033[31mError: \033[0m[{0}] Login failed!'.format(username))
         return 0
 
+# 从特定题目中加载样例
 def handle_LoadSamples (prob_id):
-    global browser
-    browser = RoboBrowser(history=True, parser='lxml')
+    # 先登录，不然进入正常的比赛了话样例出不来
+    if not handle_Login(info_data['username'], info_data['password']):
+        print("%s> Login error: username or password wrong"%info_data['username'])
+        return
+    else:
+        global browser
+        browser = RoboBrowser(history=True, parser='lxml')
+
     browser.open('https://codeforces.com/contest/%s/problem/%s' % (info_data['contest_id'], prob_id))
 
+    # 文件夹没有就建
     if not os.path.exists('%s'%info_data['contest_id']):
         os.mkdir('%s'%info_data['contest_id'])
     os.chdir('%s'%info_data['contest_id'])
     if not os.path.exists('%s'%prob_id):
         os.mkdir('%s'%prob_id)
     os.chdir('%s'%prob_id)
+
+    # 先加载输入样例
     samp_id = 1
     for input_html in browser.find_all('div', {'class': 'input'}):
         sip = SampParser('%s/%s/%s'%(psn_path, info_data['contest_id'], prob_id), 'sample%s.in'%samp_id)
         sip.feed('%s'%input_html)
         samp_id += 1
+    # 再加载输出样例
     samp_id = 1
     for output_html in browser.find_all('div', {'class': 'output'}):
         sip = SampParser('%s/%s/%s'%(psn_path, info_data['contest_id'], prob_id), 'sample%s.out'%samp_id)
@@ -108,6 +126,13 @@ def handle_LoadSamples (prob_id):
         samp_id += 1
     os.chdir('../..')
 
+    # URL 不存在，体现为啥都没拉进来
+    if not os.path.exists('%s/%s/sample1.in'%(info_data['contest_id'], prob_id)):
+        print('url: https://codeforces.com/contest/%s/problem/%s not found' % (info_data['contest_id'], prob_id))
+        os.rmdir('%s/%s'%(info_data['contest_id'], prob_id))
+        exit()
+
+# 比对两个文件在清除掉 \n \t 空格 后是否相同
 def file_cmp_should_block (file_path1, file_path2):
     file1 = open(file_path1, encoding='utf-8')
     file2 = open(file_path2, encoding='utf-8')
@@ -121,14 +146,17 @@ def file_cmp_should_block (file_path1, file_path2):
     s2 = s2.replace('\t', '')
     return s1 == s2
 
-def handle_Test (filename, prob_id):
+# 测试 filename 能否解决 prob_id
+def handle_Test (filename, prob_id, sample_id):
+    # 加载样例
     if not os.path.exists('%s/%s'%(info_data['contest_id'], prob_id)):
         handle_LoadSamples(prob_id)
     
+    # 编译 filename
     ext = filename.rsplit('.')[1]
     filename_without_ext = filename.rsplit('.')[0]
     if ext == 'cpp':
-        cmd = 'g++ %s -o %s' % (filename, filename_without_ext)
+        cmd = 'clang++ %s -o %s' % (filename, filename_without_ext)
         for s in cpp:
             cmd += ' %s'%s
         os.system(cmd)
@@ -138,6 +166,45 @@ def handle_Test (filename, prob_id):
             cmd += ' %s'%s
         os.system(cmd)
 
+    # 单测样例
+    if sample_id != None:
+        if not os.path.exists('%s/%s/%s.in'%(info_data['contest_id'], prob_id, sample_id)) or not os.path.exists('%s/%s/%s.out'%(info_data['contest_id'], prob_id, sample_id)):
+            print('Sample file {0}.in or {0}.out not exist ...'.format(sample_id))
+            return
+        file = '%s.in'%sample_id
+        samp_name = (('%s/%s/%s/%s'%(psn_path, info_data['contest_id'], prob_id, file)).rsplit('.')[0])
+        f = open(samp_name + "_my.out", 'w')
+        f.close()
+        if ext == 'cpp':
+            cmd = '{0}/{1} <\"{2}.in\" >\"{2}_my.out\"'.format(psn_path, filename_without_ext, samp_name)
+            os.system(cmd)
+        elif ext == 'java':
+            cmd = 'java {0} <\"{1}.in\" >\"{1}_my.out\"'.format(filename_without_ext, samp_name)
+            os.system(cmd)
+        elif ext == 'py':
+            cmd = 'python %s/%s'%(psn_path, filename)
+            for s in python:
+                cmd += ' %s'%s
+            cmd += ' <\"{0}.in\" >\"{0}_my.out\"'.format(samp_name)
+            os.system(cmd)
+        print('==========================[%s]'%(file.rsplit('.')[0]))
+        if file_cmp_should_block(samp_name + '.out', samp_name + '_my.out'):
+            print(" >> \033[1;32mAccept\033[0m\n")
+        else:
+            print(" >> \033[1;31mWrong Answer\033[0m\n")
+            inf = open('%s.in'%samp_name)
+            myf = open('%s_my.out'%samp_name)
+            trf = open('%s.out'%samp_name)
+            print("--- \033[1mInput:\033[0m")
+            print(inf.read())
+            print("--- \033[1mTrue Answer:\033[0m")
+            print(trf.read())
+            print("--- \033[1mYour Answer:\033[0m")
+            print(myf.read())
+        print()
+        return
+
+    # 多测样例，运行 filename 并比对
     for file in os.listdir('%s/%s'%(info_data['contest_id'], prob_id)):
         if file.rsplit('.')[1] == 'in':
             samp_name = (('%s/%s/%s/%s'%(psn_path, info_data['contest_id'], prob_id, file)).rsplit('.')[0])
@@ -171,18 +238,19 @@ def handle_Test (filename, prob_id):
                 print(myf.read())
             print()
 
+# 交题
 def handle_Submit (filename, prob_id):
     if not handle_Login(info_data['username'], info_data['password']):
         print("%s> Login error: username or password wrong"%info_data['username'])
         return
         
-    print("\033[33mUploading file %s, please wait....\033[0m"%filename)
+    print("%s> \033[33mUploading file %s, please wait....\033[0m"%(info_data['username'], filename))
     browser.open('https://codeforces.com/contest/%s/problem/%s'%(info_data['contest_id'], prob_id))
     form = browser.get_form(class_ = 'submitForm')
     try:
         form['sourceFile'] = filename
     except Exception as e:
-        print('%s not found!'%filename)
+        print('file\'%s\' or url\'https://codeforces.com/contest/%s/problem/%s\' not found!'%(filename, info_data['contest_id'], prob_id))
         return
     ext = filename.rsplit('.')[1]
     if ext == 'cpp':
@@ -195,7 +263,7 @@ def handle_Submit (filename, prob_id):
     if browser.url != 'https://codeforces.com/contest/%s/my'%info_data['contest_id']:
         print('%s> Duplicate filed!'%info_data['username'])
         return
-    
+    # 等待结果
     while True:
         browser.open('https://codeforces.com/contest/%s/my'%info_data['contest_id'])
         fir_submit = browser.find('td', {'class', 'status-cell status-small status-verdict-cell'})
@@ -219,7 +287,8 @@ def handle_Submit (filename, prob_id):
 @click.argument('contest_id')
 @click.argument('problem_id')
 @click.argument('opt')
-def main (filename, contest_id, problem_id, opt):
+@click.argument('sample_id',required=False, default=None)
+def main (filename, contest_id, problem_id, opt, sample_id):
     global info_data
     """数据恢复"""
     info_file = open(dir_path + '/info.json', 'r')
@@ -229,7 +298,7 @@ def main (filename, contest_id, problem_id, opt):
     if opt == 's':
         handle_Submit(filename, problem_id)
     elif opt == 't':
-        handle_Test(filename, problem_id)
+        handle_Test(filename, problem_id, sample_id)
 
     """数据备份"""
     info_json = json.dumps(info_data, sort_keys=False, indent=4, separators=(',', ': '))
